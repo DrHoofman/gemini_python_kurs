d19507121c14f10976c1cbaa0124d33acd3047c4
DrHoofman <stefan.schumm@posteo.de>
2025-01-02 19:00:27 +0100

    Variable zugefügt

--- grundlagen/schritt_2_variablen_datentypen.py
+++ grundlagen/schritt_2_variablen_datentypen.py
@@ -2,7 +2,7 @@ name = "Stefan "           # String: Text
 alter = 65                 # Integer: Ganzzahl  
 groesse = 1.87             # Float: Kommazahl
 ist_programmierer = True   # Boolean: Wahrheitswert (True/False)
-
+zahl_der_Datentypen = 4    # Integer: Ganzzahl
 # Ausgabe der Variablen
 print("Name:", name)
 print("Alter:", alter)
@@ -14,3 +14,4 @@ print("Der Datentyp von 'name' ist:", type(name))
 print("Der Datentyp von 'alter' ist:", type(alter))
 print("Der Datentyp von 'groesse' ist:", type(groesse))
 print("Der Datentyp von 'ist_programmierer' ist:", type(ist_programmierer)) 
+print("das sind insgesamt "(zahl_der_Datentypen))
