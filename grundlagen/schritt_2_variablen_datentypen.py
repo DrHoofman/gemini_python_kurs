588af88c7be9c94f82c1ef7727026bc6b95584bc
DrHoofman <stefan.schumm@posteo.de>
2025-01-02 18:51:33 +0100

    datentypen

---
+++ grundlagen/schritt_2_variablen_datentypen.py
@@ -0,0 +1,16 @@
+name = "Stefan "           # String: Text
+alter = 65                 # Integer: Ganzzahl  
+groesse = 1.87             # Float: Kommazahl
+ist_programmierer = True   # Boolean: Wahrheitswert (True/False)
+
+# Ausgabe der Variablen
+print("Name:", name)
+print("Alter:", alter)
+print("Größe:", groesse, "Meter")
+print("Ist Programmierer?", ist_programmierer)
+
+# Überprüfen der Datentypen
+print("Der Datentyp von 'name' ist:", type(name))
+print("Der Datentyp von 'alter' ist:", type(alter))
+print("Der Datentyp von 'groesse' ist:", type(groesse))
+print("Der Datentyp von 'ist_programmierer' ist:", type(ist_programmierer)) 
