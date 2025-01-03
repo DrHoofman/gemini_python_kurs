b7f020daa10c720e13c4b3be4754baec10bc6a40
DrHoofman <stefan.schumm@posteo.de>
2025-01-03 18:09:27 +0100

    Dictionaries hinzugefügt und getestet

--- grundlagen/schritt_4_dictionaries.py
+++ grundlagen/schritt_4_dictionaries.py
@@ -1,9 +1,13 @@
 # Ein Wörterbuch erstellen
 person = {
     "name": "Stefan",
-    "alter": 65,
-    "groesse": 1.80,
-    "ist_programmierer": True
+    "alter": 66,
+    "groesse": 1.87,
+    "ist_programmierer": True,
+    "Wohnort": "Altendorf",
+    "Postleitzahl": 96127,
+    "Straße": "Pointstrasse",
+    "Hausnummer": 2,
 }
 
 # Das gesamte Wörterbuch ausgeben
@@ -12,3 +16,9 @@ print("\nPersonendaten:", person)
 # Zugriff auf einzelne Werte
 print("Name der Person:", person["name"])
 print("Alter der Person:", person["alter"])
+print("Wohnort der Person:", person["Wohnort"])
+
+# Schleife durch das Wörterbuch
+print("\nAlle Daten der Person:")
+for schluessel, wert in person.items():
+    print(f"{schluessel}: {wert}")
