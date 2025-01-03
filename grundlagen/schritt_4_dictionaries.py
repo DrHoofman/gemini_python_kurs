1d43de7f92e648b22ca56f352757d2883452d8cb
DrHoofman <stefan.schumm@posteo.de>
2025-01-03 11:09:08 +0100

    Woerterbuch erstellen

---
+++ grundlagen/schritt_4_dictionaries.py
@@ -0,0 +1,14 @@
+# Ein Wörterbuch erstellen
+person = {
+    "name": "Stefan",
+    "alter": 65,
+    "groesse": 1.80,
+    "ist_programmierer": True
+}
+
+# Das gesamte Wörterbuch ausgeben
+print("\nPersonendaten:", person)
+
+# Zugriff auf einzelne Werte
+print("Name der Person:", person["name"])
+print("Alter der Person:", person["alter"])
