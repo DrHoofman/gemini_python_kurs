526155441548f4ca9c699b6ddbe6d0d2d3c581d9
DrHoofman <stefan.schumm@posteo.de>
2025-01-05 19:00:11 +0100

    schleifen

--- grundlagen/uebung_002.py
+++ grundlagen/uebung_002.py
@@ -28,3 +28,9 @@ for zahl in range(10):
     if zahl % 2 == 0:
         continue # Überspringt gerade Zahlen
     print(f"Ungerade Zahl: {zahl}")
+
+for zahl in range(1,21):
+    if zahl == 7:
+        print("die Zahl ist", zahl)
+    if zahl == 15:
+        print("die Schleife wurde bei ", zahl, "gestoppt")  
