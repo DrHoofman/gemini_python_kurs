758fa32f8452323de8f92db54bbd0940d682b397
DrHoofman <stefan.schumm@posteo.de>
2025-01-06 20:32:54 +0100

    einfache Funktion ohne Parameter

---
+++ grundlagen/schritt_5_funktionen.py
@@ -0,0 +1,15 @@
+# allgemeine Syntx für Funktionen
+# def funktionsname(parameter):
+#     #Codeblock
+#     return wert
+#	•	def: Schlüsselwort zum Definieren einer Funktion.
+#	•	funktionsname: Name der Funktion.
+#	•	parameter: Optional, Werte, die der Funktion übergeben werden.
+#	•	return: Optional, gibt einen Wert zurück.
+
+#Einfache Funktionen ohne Parameter
+def hallo():
+    print("Hallo, Stefan!")
+
+# Funktionsaufruf
+hallo()
