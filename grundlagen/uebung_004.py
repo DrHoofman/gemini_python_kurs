e827563db93d6dd33a56adbade02374312cc17ce
DrHoofman <stefan.schumm@posteo.de>
2025-01-06 18:36:20 +0100

    Nur die Schlüssel ausgeben

--- grundlagen/uebung_004.py
+++ grundlagen/uebung_004.py
@@ -18,3 +18,16 @@ for zahl in zahlen:
 farben = ["rot", "grün", "blau", "gelb"]
 for index, farbe in enumerate(farben):
     print(f"Farbe an Position {index}: {farbe}")
+
+#Schleifen mit dictionaries
+#Beispiel: Ein Wörterbuch ausgeben
+person = {
+    "Name": "Stefan",
+    "Alter": 65,
+    "Wohnort": "Altendorf"
+}
+for schluessel, wert in person.items():
+    print(f"{schluessel}: {wert}")  # Ausgabe: Name: Stefan, Alter: 65, Wohnort: Altendorf
+
+for schluessel in person:
+    print(f"Schlüssel: {schluessel}")
