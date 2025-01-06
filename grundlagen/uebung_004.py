66caf735496b5c27792009e5d8ff16c5677dbafd
DrHoofman <stefan.schumm@posteo.de>
2025-01-06 14:29:54 +0100

    Schleifen mit Listen

---
+++ grundlagen/uebung_004.py
@@ -0,0 +1,8 @@
+namen = ["Stefan", "Lisa", "Lucca", "Lea", "Trang", "Nga", "Quang"]
+for name in namen:
+    print(f"hallo, {name}!")
+
+zahlen = [3, 7, 12, 15]
+
+for i in range(len(zahlen)):
+    print(f"index {i}: Wert = {zahlen[i]}")
