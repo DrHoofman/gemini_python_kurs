a317942ab72710f4c3ed10a81890a69a6bb68f45
DrHoofman <stefan.schumm@posteo.de>
2025-01-06 14:45:56 +0100

    enumerate

--- grundlagen/uebung_004.py
+++ grundlagen/uebung_004.py
@@ -6,3 +6,15 @@ zahlen = [3, 7, 12, 15]
 
 for i in range(len(zahlen)):
     print(f"index {i}: Wert = {zahlen[i]}")
+
+# Aufgabe 1: Schreibe eine Schleife, die die Summe der Zahlen in der Liste zahlen berechnet und ausgibt
+zahlen = [3, 7, 12, 15]
+summe = 0
+for zahl in zahlen:
+    summe += zahl
+    print(f"summe der Zahlen: {summe}")
+
+# enumerate()  für Listen (Index und Wert kombinieren)
+farben = ["rot", "grün", "blau", "gelb"]
+for index, farbe in enumerate(farben):
+    print(f"Farbe an Position {index}: {farbe}")
