cdbfb96d51895b1d8db6063eefbfbb33e4792842
DrHoofman <stefan.schumm@posteo.de>
2025-01-06 20:21:22 +0100

    Leerzeilen

--- grundlagen/uebung_004.py
+++ grundlagen/uebung_004.py
@@ -1,11 +1,13 @@
 namen = ["Stefan", "Lisa", "Lucca", "Lea", "Trang", "Nga", "Quang"]
 for name in namen:
     print(f"hallo, {name}!")
+print("\n")
 
 zahlen = [3, 7, 12, 15]
 
 for i in range(len(zahlen)):
     print(f"index {i}: Wert = {zahlen[i]}")
+print("\n")
 
 # Aufgabe 1: Schreibe eine Schleife, die die Summe der Zahlen in der Liste zahlen berechnet und ausgibt
 zahlen = [3, 7, 12, 15]
@@ -13,6 +15,7 @@ summe = 0
 for zahl in zahlen:
     summe += zahl
     print(f"summe der Zahlen: {summe}")
+
 
 # enumerate()  für Listen (Index und Wert kombinieren)
 farben = ["rot", "grün", "blau", "gelb"]
